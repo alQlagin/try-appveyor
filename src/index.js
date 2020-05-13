33b1ea47f663c144402e75717d9f9d65da89d164
Alex Kulagin <al.qlagin@gmail.com>
2020-05-13 13:40:25 +0500

    configure webpack

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const main = document.createElement("main");
+main.textContent = "Hello appveyor";
+document.body.appendChild(main);
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.js",
+  },
+  plugins: [new HtmlWebPackPlugin({})],
+};
